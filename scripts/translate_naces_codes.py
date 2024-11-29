8a16806c9266126b223437403ac5a1852fe99617
David Neuroth <d.neuroth@fz-juelich.de>
2024-11-29 22:41:08 +0100

    added nace codes, a conversion script and a nace to location mapping

---
+++ scripts/translate_naces_codes.py
@@ -0,0 +1,28 @@
+"""A script for parsing, filtering and storing the nace codes in a suitable format for mapping buildigns."""
+
+import json
+import pandas as pd
+
+path = "data/nace_codes.txt"
+
+data = pd.read_csv(path, sep=" - ", header=1)
+
+# remove leading letters
+data.iloc[:, 0] = data.iloc[:, 0].map(lambda s: s[1:] if len(s) > 1 else s)
+
+# remove all but the pure letter codes and single number codes
+data = data.loc[~data["Code"].str.contains(".", regex=False)]
+data = data.loc[data["Code"] != ""]
+
+print(data)
+
+# create a JSON file mapping each nace to its description
+d = dict(zip(data["Code"], data["Description"]))
+with open("data/nace_code_descriptions.json", "w+") as f:
+    json.dump(d, f)
+
+d2 = {v: [] for k, v in d.items() if k.isdigit()}
+with open("data/nace_codes_to_locations.json", "w+") as f:
+    json.dump(d2, f)
+
+# data.to_csv("nace_codes_filtered.txt", index=False)
