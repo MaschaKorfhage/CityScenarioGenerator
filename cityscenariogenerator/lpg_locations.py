43da9f834227746ea0260b942ae4e55d38f1d90b
David Neuroth <d.neuroth@fz-juelich.de>
2025-02-17 22:51:52 +0100

    added more categories of LPG locations

--- cityscenariogenerator/lpg_locations.py
+++ cityscenariogenerator/lpg_locations.py
@@ -4,38 +4,30 @@ A module for loading the locations used in the LoadProfileGenerator.
 
 from pathlib import Path
 
-
+#: directory containing the location files
 LOCATION_DIR = Path("data/lpg_locations")
 
 
 def load_location_set(path: Path) -> set[str]:
+    """
+    Loads a list of LoadProfileGenerator locations from a text file.
+
+    :param path: path to the file
+    :return: set of location names
+    """
     with open(path, "r") as f:
         all_locations = f.read()
     return set(all_locations.splitlines())
 
 
-def get_lpg_remote_locations() -> set[str]:
-    """
-    Returns a list of all LPG locations that are not at home,
-    and that therefore requrire a POI.
-
-    :return: set of location names
-    """
-    return load_location_set(LOCATION_DIR / "all_remote.txt")
-
-
-def get_lpg_work_locations() -> set[str]:
-    """
-    Returns a list of all LPG locations that are used for
-    work activities, including volunteer work, but excluding
-    home office. This is a subset of all LPG remote locations.
-
-    :return: set of location names
-    """
-    return load_location_set(LOCATION_DIR / "work.txt")
-
-
 class LpgLocations:
-    ALL = get_lpg_remote_locations()
-    WORK = get_lpg_work_locations()
+    """
+    Stores different sets of LPG locations, according to different categories.
+    Some of the sets are not mutually exclusive.
+    """
+
+    ALL = load_location_set(LOCATION_DIR / "all_remote.txt")
+    WORK = load_location_set(LOCATION_DIR / "work.txt")
     NON_WORK = ALL - WORK
+    RESIDENTIAL = load_location_set(LOCATION_DIR / "residential.txt")
+    NO_BUILDING = load_location_set(LOCATION_DIR / "no_building.txt")
