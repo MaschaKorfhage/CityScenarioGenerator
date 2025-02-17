275e598f67fc03d7e0c4a8a5d81cae5feac9004b
David Neuroth <d.neuroth@fz-juelich.de>
2025-02-17 18:42:57 +0100

    added setup.py to make project pip installable

---
+++ README.md
@@ -0,0 +1,3 @@
+# CityScenarioGenerator
+
+CityScenarioGenerator is a tool designed to create city scenarios for simulation with the LoadProfileGenerator. It extracts residential and non-residential buildings from BUILDA, maps them to corresponding categories in the LPG model, and saves everything in a directory of config files.
---
+++ setup.py
@@ -0,0 +1,36 @@
+import os
+import setuptools
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
+with open(os.path.join(dir_path, "requirements.txt")) as f:
+    required_packages = f.read().splitlines()
+with open(os.path.join(dir_path, "README.md"), "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="cityscenariogenerator",
+    version="0.1.0",
+    author="David Neuroth",
+    author_email="d.neuroth@fz-juelich.de",
+    description="A city scenario generator for the LoadProfileGenerator",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="TODO",
+    include_package_data=True,
+    packages=setuptools.find_packages(),
+    install_requires=required_packages,
+    setup_requires=["setuptools-git"],
+    license="MIT",
+    classifiers=[
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Framework :: Dash",
+        "Topic :: Scientific/Engineering",
+    ],
+    keywords=[
+        "city simulation",
+        "occupant behavior",
+        "load profile generator",
+    ],
+)
