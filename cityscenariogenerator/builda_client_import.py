e46f354802ec4e1ba2fd7c7b88f5b75283d9a657
David Neuroth <d.neuroth@fz-juelich.de>
2025-03-17 22:11:09 +0100

    added local pickle file caching of BUILDA results

--- cityscenariogenerator/builda_client_import.py
+++ cityscenariogenerator/builda_client_import.py
@@ -4,14 +4,18 @@ from collections import Counter
 import logging
 import os
 from pathlib import Path
+import pickle
 from dotenv import load_dotenv
 
 from builda_client.dev_client import (  # type: ignore
     BuildaDevClient,
     Phase,
+    Building,
     ResidentialBuilding,
     NonResidentialBuilding,
 )
+
+from cityscenariogenerator import utils
 
 
 # load credentials for the BuildaDevClient
@@ -29,22 +33,85 @@ def get_builda_devclient():
     )
 
 
+def get_cache_filename(search_args, query_type) -> Path:
+    """
+    Determine the cache filename for a given query.
+
+    :param search_args: BUILDA query
+    :param query_type: type of query
+    :return: cache filename
+    """
+    query_text = utils.descriptive_query_text(search_args)
+    cache_file = Path("cache") / f"{query_type}_{query_text}.pkl"
+    return cache_file
+
+
+def cache_builda_result(
+    search_args: dict, query_type: str, data: list[Building]
+) -> None:
+    """
+    Caches the result of a BUILDA query in a local pickle file.
+
+    :param search_args: BUILDA query
+    :param query_type: the type of query, e.g., 'residential'
+    :param data: the result of the query that will be cached
+    """
+    cache_file = get_cache_filename(search_args, query_type)
+    cache_file.parent.mkdir(parents=True, exist_ok=True)
+    if cache_file.exists():
+        logging.debug(f"Overwriting BUILDA cache file: {cache_file}")
+    with open(cache_file, "wb") as f:
+        pickle.dump(data, f)
+
+
+def load_builda_cache(search_args: dict, query_type: str) -> list[Building] | None:
+    """
+    Loads a cached BUILDA query result from a local pickle file.
+
+    :param search_args: BUILDA query
+    :param query_type: type of the query
+    :return: the cached result objects
+    """
+    cache_file = get_cache_filename(search_args, query_type)
+    try:
+        with open(cache_file, "rb") as f:
+            data = pickle.load(f)
+            return data
+    except FileNotFoundError:
+        logging.debug(f"No cache file found: {cache_file}")
+        return None
+
+
 def get_nonresidential_buildings(
-    search_args: dict,
+    search_args: dict, use_cache: bool = True
 ) -> list[NonResidentialBuilding]:
+    # check whether the result is already cached
+    if use_cache and (cached := load_builda_cache(search_args, "nonres")):
+        return cached
     client = get_builda_devclient()
 
     nonres_buildings = client.get_non_residential_buildings(
         **search_args, exclude_auxiliary=True
     )
     logging.info(f"Non-residential buildings in {search_args}: {len(nonres_buildings)}")
+    if use_cache:
+        # cache the result
+        cache_builda_result(search_args, "nonres", nonres_buildings)
     return nonres_buildings
 
 
-def get_residential_buildings(search_args: dict) -> list[ResidentialBuilding]:
+def get_residential_buildings(
+    search_args: dict, use_cache: bool = True
+) -> list[ResidentialBuilding]:
+    # check whether the result is already cached
+    if use_cache and (cached := load_builda_cache(search_args, "res")):
+        return cached
     client = get_builda_devclient()
     res_buildings = client.get_residential_buildings(**search_args)
     logging.info(f"Residential buildings in {search_args}: {len(res_buildings)}")
+    if use_cache:
+        # cache the result
+        cache_builda_result(search_args, "res", res_buildings)
     return res_buildings
 
 
