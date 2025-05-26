a018d3b928428df9ffcd5b57359f7a5ab939e90b
David Neuroth <d.neuroth@fz-juelich.de>
2025-05-26 11:39:52 +0200

    changed paths for population validation plots

--- cityscenariogenerator/plots/population_comparison.py
+++ cityscenariogenerator/plots/population_comparison.py
@@ -20,11 +20,11 @@ def population_statistics(params: ScenarioParams, result_dir: Path):
     # load validation data if it exists
     validation_file = params.input_data_dir() / "validation_data/population.json"
     if not validation_file.is_file():
-        logging.info(f"No population validation data found.")
+        logging.info("No population validation data found.")
         return
     with open(validation_file, "r") as f:
         validation_stats: dict[str, dict[str, int]] = json.load(f)
-    
+
     # load the population statistics of the generated scenario
     scenario_file = params.result_directory / "statistics/person_statistics.json"
     with open(scenario_file, "r") as f:
@@ -32,19 +32,20 @@ def population_statistics(params: ScenarioParams, result_dir: Path):
 
     for measure, validation in validation_stats.items():
         if not isinstance(validation, dict):
-            continue # this is metadata, skip this
-        
-        if not measure in scenario_stats:
+            continue  # this is metadata, skip this
+
+        if measure not in scenario_stats:
             logging.warning(f"Missing population statistics measure: {measure}")
             continue
-        
-        data = pd.DataFrame({
-            'Validation': validation,
-            'Scenario': scenario_stats[measure]
-        })
-        data_subdir = result_dir / "population_validation"
-        data_subdir.mkdir(parents=True, exist_ok=True)
-        data.to_csv(data_subdir / f"{measure}.csv")
+
+        data = pd.DataFrame(
+            {"Validation": validation, "Scenario": scenario_stats[measure]}
+        )
+        plot_result_dir = result_dir / "population_validation"
+        plot_result_dir.mkdir(parents=True, exist_ok=True)
+        # data_subdir = plot_result_dir / "data"
+        # data_subdir.mkdir(parents=True, exist_ok=True)
+        # data.to_csv(data_subdir / f"{measure}.csv")
 
         fig = plt.figure()
         ax = fig.add_subplot(1, 1, 1)
@@ -53,7 +54,16 @@ def population_statistics(params: ScenarioParams, result_dir: Path):
         df_long = data.melt(id_vars=measure, var_name="Dataset", value_name=y_title)
 
         sns.barplot(x=measure, ax=ax, y=y_title, hue="Dataset", data=df_long)
-        ax.tick_params(axis='x', labelrotation=90)
+        ax.tick_params(axis="x", labelrotation=90)
         fig.align_labels()
         fig.tight_layout()
-        fig.savefig(result_dir / f"population_validation_{measure}.svg")
+        fig.savefig(plot_result_dir / f"{measure}.svg")
+
+
+if __name__ == "__main__":
+
+    result_dir = Path("R:/phd_dir/data/city_scenarios/scenario_julich")
+
+    params = ScenarioParams({"city": "Jülich"}, result_dir, Path())
+    population_statistics(params, params.result_directory / "plots")
+    print("Population validation plots created.")
