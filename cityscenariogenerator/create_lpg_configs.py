da6d5593e39455e5575b873630ac3d6ebe1df711
David Neuroth <d.neuroth@fz-juelich.de>
2025-10-29 01:53:53 +0100

    fix missing capacity for site planning pharmacies

--- cityscenariogenerator/create_lpg_configs.py
+++ cityscenariogenerator/create_lpg_configs.py
@@ -86,13 +86,12 @@ def create_configs_from_buildings(
     # determine which POIs each person visits
     config_creator.create_poi_preferences(generate_test_routes)
 
-    config_creator.add_default_queuecapacity("Pharmacy", 2)
-
     # optional custom instance export
     site_export.create_pharmacy_instances(params, config_creator)
 
     # optional application of site planning results
-    # site_export.apply_eplpo_pharmacy_results(params, config_creator)
+
+    config_creator.add_default_queuecapacity("Pharmacy", 2)
 
     if generate_test_routes:
         # create random routes for testing
