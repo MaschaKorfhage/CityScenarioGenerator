fc570ba7956f4d2b1ed8e82bd48539240282a88d
Mascha Korfhage <mascha.korfhage@gmail.com>
2026-06-10 17:31:37 +0200

    Für mich irrelevante Zeile auskommentiert

--- cityscenariogenerator/create_lpg_configs.py
+++ cityscenariogenerator/create_lpg_configs.py
@@ -96,7 +96,7 @@ def create_configs_from_buildings(
     config_creator.create_poi_preferences(generate_test_routes)
 
     # optional custom instance export
-    site_export.create_pharmacy_instances(params, config_creator)
+    #site_export.create_pharmacy_instances(params, config_creator)
 
     # optional application of site planning results
     # site_export.apply_eplpo_pharmacy_results(params, config_creator)
